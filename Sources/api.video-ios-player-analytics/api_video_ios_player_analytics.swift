de47b9a700b9e6dfdac6b9def1f84fdd72ed6c4a
romain petit <contact.romain.petit@gmail.com>
2022-02-02 11:20:35 +0100

    init package

---
+++ Sources/api.video-ios-player-analytics/api_video_ios_player_analytics.swift
@@ -0,0 +1,6 @@
+public struct api_video_ios_player_analytics {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/api.video-ios-player-analyticsTests/api_video_ios_player_analyticsTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import api_video_ios_player_analytics
+
+final class api_video_ios_player_analyticsTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(api_video_ios_player_analytics().text, "Hello, World!")
+    }
+}
