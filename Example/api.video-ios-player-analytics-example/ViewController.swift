b6944268ad4073597283ba1c3c9eefe82bc7a1d0
romain petit <contact.romain.petit@gmail.com>
2022-02-17 09:48:37 +0100

    feat(sample) add possibility to change video url

--- Example/api.video-ios-player-analytics-example/ViewController.swift
+++ Example/api.video-ios-player-analytics-example/ViewController.swift
@@ -11,52 +11,28 @@ import AVKit
 import api_video_ios_player_analytics
 
 class ViewController: UIViewController {
+    @IBOutlet weak var videoUrlTextField: UITextField!
     var api: api_video_ios_player_analytics?
     var option: Options?
     var player: AVPlayer? = nil
     let controller = AVPlayerViewController()
+    private var isFirstPlay = true
+    
+    private var videoUrl = "https://cdn.api.video/vod/vi7VQtn7FQgpxfF3s6cCgO8H/hls/manifest.m3u8"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         //api_video_ios_player_analytics().schedule()
-        let liveStr = "live"
-        let vodStr = "vod"
-        let vODStr = "VOD"
-        let toto = "toto"
-        var videoTypeLive: VideoType?
-        var videoTypeVod: VideoType?
-        var videoTypeVOD: VideoType?
-        var videoTypeToto: VideoType?
-        let session = Session.buildLiveStreamSession(sessionId: "id", loadedAt: "ajd", livestreamId: "icjeioheoi", referrer: "ref", metadata: [[:]])
-        print(session)
-        
-        //        guard let videoType = toto.toVideoType() else { return VideoType.LIVE }
+        videoUrlTextField.text = videoUrl
         do{
-            videoTypeLive = try liveStr.toVideoType()
-            print(videoTypeLive ?? "error")
-            videoTypeVod = try vodStr.toVideoType()
-            print(videoTypeVod ?? "error")
-            videoTypeVOD = try vODStr.toVideoType()
-            print(videoTypeVOD ?? "error")
-            videoTypeToto = try toto.toVideoType()
-            print(videoTypeToto as Any)
-            
-        } catch{
-            print(error)
-        }
-        do{
-            option = try Options(mediaUrl: "https://cdn.api.video/vod/vi7VQtn7FQgpxfF3s6cCgO8H/hls/manifest.m3u8", metadata: [["string 1": "String 2"], ["string 3": "String 4"]], onSessionIdReceived: { (id) in
+            option = try Options(mediaUrl: videoUrl, metadata: [["string 1": "String 2"], ["string 3": "String 4"]], onSessionIdReceived: { (id) in
                 print("session ID : \(id)")
             })
-            
-            
-            
         }catch{
             print("error with the url")
         }
         print(option!.videoInfo.videoType.rawValue)
         api = api_video_ios_player_analytics(options: option!)
-        // Do any additional setup after loading the view.
     }
     
     private func setupAVPlayer() {
@@ -69,7 +45,7 @@ class ViewController: UIViewController {
     @objc func playerDidFinishPlaying(note: NSNotification) {
         print("dismiss player")
         self.controller.dismiss(animated: true)
-        }
+    }
     
     override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
         if object as AnyObject? === player {
@@ -79,19 +55,18 @@ class ViewController: UIViewController {
                         print("ready done")
                     }
                 }
-            } else if keyPath == "timeControlStatus" {
-                if #available(iOS 10.0, *) {
-                    if player!.timeControlStatus == .playing {
-                        print("it's Playing")
-                    } else {
-                        print("it's not Playing")
-                    }
-                }
             } else if keyPath == "rate" {
                 if player!.rate > 0 {
-                    self.api!.play {
-                        print("api play")
+                    if(isFirstPlay){
+                        self.api!.play {
+                            print("api play")
+                        }
+                    }else{
+                        self.api!.resume {
+                            print("api resume")
+                        }
                     }
+                    
                 } else {
                     self.api!.pause {
                         print("api pause")
@@ -105,7 +80,9 @@ class ViewController: UIViewController {
     @IBAction func playVideo(_ sender: Any) {
         
         // Create an AVPlayer, passing it the HTTP Live Streaming URL.
-        player = AVPlayer(url: URL(string: "https://cdn.api.video/vod/vi6H2fJmGLjUX2novcT0EyWs/hls/manifest.m3u8")!)
+        player = AVPlayer(playerItem: nil)
+        
+        player?.replaceCurrentItem(with: AVPlayerItem(url: URL(string: videoUrlTextField.text!)!))
         
         // Create a new AVPlayerViewController and pass it a reference to the player.
         
@@ -114,10 +91,10 @@ class ViewController: UIViewController {
         setupAVPlayer()
         
         
-        // Modally present the player and call the player's play() method when complete.
         present(controller, animated: true) {
                     
         }
+        
     }
     
 }
