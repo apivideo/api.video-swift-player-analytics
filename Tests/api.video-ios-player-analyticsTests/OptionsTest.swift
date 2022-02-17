c0673f385903084b379a26883fe154f6604a10f2
romain petit <contact.romain.petit@gmail.com>
2022-02-17 09:50:33 +0100

    wip(test) test media url

---
+++ Tests/api.video-ios-player-analyticsTests/OptionsTest.swift
@@ -0,0 +1,36 @@
+//
+//  Options.swift
+//
+
+import XCTest
+@testable import api_video_ios_player_analytics
+
+class OptionsTest: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    func testValidVodUrl() throws{
+        let option = try Options(mediaUrl: "https://cdn.api.video/vod/vi5oDagRVJBSKHxSiPux5rYD/hls/manifest.m3u8", metadata: [[:]], onSessionIdReceived: nil, onPing: nil)
+        XCTAssertEqual(option.videoInfo.videoId, "vi5oDagRVJBSKHxSiPux5rYD")
+        XCTAssertEqual(option.videoInfo.videoType, VideoType.VOD)
+        XCTAssertEqual(option.videoInfo.pingUrl, "https://collector.api.video/vod")
+    }
+    
+    func testValidLiveUrl() throws{
+        let option = try Options(mediaUrl: "https://live.api.video/li6Anin2CG1eWirOCBnvYDzI.m3u", metadata: [[:]], onSessionIdReceived: nil, onPing: nil)
+        XCTAssertEqual(option.videoInfo.videoId, "li6Anin2CG1eWirOCBnvYDzI")
+        XCTAssertEqual(option.videoInfo.videoType, VideoType.LIVE)
+        XCTAssertEqual(option.videoInfo.pingUrl, "https://collector.api.video/live")
+    }
+    
+    func testInvalidUrl() throws{
+        XCTAssertThrowsError(try Options(mediaUrl: "https://mydomain/video.m3u8", metadata: [[:]], onSessionIdReceived: nil, onPing: nil))
+    }
+
+}
