bce91ed22646f352f57311b876c181023bdd6805
romain petit <contact.romain.petit@gmail.com>
2022-02-22 16:45:54 +0100

    fix() http request

--- Sources/TasksExecutor.swift
+++ Sources/TasksExecutor.swift
@@ -7,17 +7,8 @@ public class TasksExecutor: TasksExecutorProtocol{
         task = session.dataTask(with: request, completionHandler: {data, response, error -> Void in
             let httpResponse = response as? HTTPURLResponse
             let statuscode = httpResponse?.statusCode
-            switch statuscode!{
-            case 200 ... 299:
-                task?.cancel()
-                completion(data, error)
-            case 400, 401, 404:
-                task?.cancel()
-                completion(data,error)
-            default:
-                task?.cancel()
-                completion(data,error)
-            }
+            task?.cancel()
+            completion(data, error)
             if(group != nil){
                 group!.leave()
             }
