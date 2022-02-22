eb85e6d052f7ae011eb7fdf294cf7202c4e75ebe
romain petit <contact.romain.petit@gmail.com>
2022-02-22 09:24:12 +0100

    fix() change package name

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "api.video-ios-player-analytics",
+    name: "ApiVideoIosPlayerAnalytics",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "api.video-ios-player-analytics",
-            targets: ["api.video-ios-player-analytics"]),
+            name: "ApiVideoIosPlayerAnalytics",
+            targets: ["ApiVideoIosPlayerAnalytics"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,10 +19,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "api.video-ios-player-analytics",
+            name: "ApiVideoIosPlayerAnalytics",
             dependencies: []),
         .testTarget(
-            name: "api.video-ios-player-analyticsTests",
-            dependencies: ["api.video-ios-player-analytics"]),
+            name: "ApiVideoIosPlayerAnalyticsTests",
+            dependencies: ["ApiVideoIosPlayerAnalytics"]),
     ]
 )
