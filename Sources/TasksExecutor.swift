20d404a0cfe8bf5ba9679fd048fdbf8ed8fec60e
romain petit <contact.romain.petit@gmail.com>
2022-02-24 09:57:34 +0100

    fix() delete unused func

--- Sources/TasksExecutor.swift
+++ Sources/TasksExecutor.swift
@@ -2,22 +2,12 @@
 import Foundation
 public class TasksExecutor: TasksExecutorProtocol{
     private let decoder = JSONDecoder()
-    public func execute(session: URLSession, request: URLRequest, group: DispatchGroup?, completion: @escaping (Data?, Error?) -> ()){
-        var task: URLSessionTask?
-        task = session.dataTask(with: request, completionHandler: {data, response, error -> Void in
-            let httpResponse = response as? HTTPURLResponse
-            let statuscode = httpResponse?.statusCode
-            task?.cancel()
-            completion(data, error)
-            if(group != nil){
-                group!.leave()
-            }
-        })
-        task!.resume()
-    }
     public func execute(session: URLSession, request: URLRequest, completion: @escaping (Data?, Error?) -> ()){
-        execute(session: session, request: request, group: nil){(data, error) in
+        
+        let task = session.dataTask(with: request){(data, response, error) in
             completion(data, error)
         }
+        task.resume()
     }
 }
+
--- Sources/TasksExecutorProtocol.swift
+++ Sources/TasksExecutorProtocol.swift
@@ -1,8 +1,6 @@
 
 import Foundation
-public protocol TasksExecutorProtocol{
-    func execute(session: URLSession, request: URLRequest, group: DispatchGroup?, completion: @escaping (Data?, Error?) -> ())
-    
+public protocol TasksExecutorProtocol{    
     func execute(session: URLSession, request: URLRequest, completion: @escaping (Data?, Error?) -> ())
     
 }
