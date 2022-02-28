5e2e4260cd897f51166fad1c9b25297ef7c09796
romain petit <contact.romain.petit@gmail.com>
2022-02-28 11:25:25 +0100

    fix() tests

--- Tests/mock/MockedTasksExecutor.swift
+++ Tests/mock/MockedTasksExecutor.swift
@@ -1,27 +1,21 @@
 
 import Foundation
 import ApiVideoPlayerAnalytics
-class MockedTasksExecutor: TasksExecutorProtocol {
+class MockedTasksExecutor: TasksExecutorProtocol {    
     static func execute(session: URLSession, request: URLRequest, completion: @escaping (Data?, Error?) -> ()) {
-        execute(session: session, request: request){(data, error) in
-            guard let url = Bundle(for: MockedTasksExecutor.self).url(forResource: "uploadSuccess", withExtension: "json"),
-                  let data = try? Data(contentsOf: url)else{
-                      completion(nil, nil)
-                      return
-                  }
-            
-            completion(data, nil)
+        guard let url = Bundle(for: MockedTasksExecutor.self).url(forResource: "uploadSuccess", withExtension: "json")else{
+            completion(nil, nil)
+            return
         }
+        guard let data = try? Data(contentsOf: url)else{
+            completion(nil, nil)
+            return
+        }
+        completion(data, nil)
     }
     
     public static func executefailed(session: URLSession, request: URLRequest, completion: @escaping (Data?, Error?) -> ()){
-        execute(session: session, request: request){(data, error) in
-            guard let url = Bundle(for: MockedTasksExecutor.self).url(forResource: "uploadError", withExtension: "json"),
-                  let data = try? Data(contentsOf: url)else{
-                      completion(nil, nil)
-                      return
-                  }
-            completion(data, error)
-        }
+        let error = UrlError.malformedUrl("error media url")
+        completion(nil, error)
     }
 }
