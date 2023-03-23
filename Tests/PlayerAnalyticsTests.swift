aa425a92960d07fbd98213d4943c12a6ad10c7df
ThibaultBee <37510686+ThibaultBee@users.noreply.github.com>
2023-03-23 10:58:40 +0100

    chore(test): add test for live, for former cdn url and avoid using real id for unit test

--- Tests/PlayerAnalyticsTests.swift
+++ Tests/PlayerAnalyticsTests.swift
@@ -4,38 +4,11 @@ import XCTest
 
 @available(iOS 10.0, *)
 final class PlayerAnalyticsTests: XCTestCase {
-    func testSuccessTask() throws {
-        guard
-            let url = Bundle(for: MockedTasksExecutor.self).url(
-                forResource: "uploadSuccess", withExtension: "json"
-            ),
-            let returnData = try? Data(contentsOf: url)
-        else {
-            return
-        }
+    func testPlayVodSuccess() throws {
+        let expectationSuccess = expectation(description: "Expect a success POST of play event")
 
-        let request = RequestsBuilder().postClientUrlRequestBuilder(apiPath: "url")
-        let session = RequestsBuilder().buildUrlSession()
-        MockedTasksExecutor.execute(session: session, request: request) { data, response in
-            XCTAssertEqual(returnData, data)
-            XCTAssertNil(response)
-        }
-    }
-
-    func testErrorTask() throws {
-        let request = RequestsBuilder().postClientUrlRequestBuilder(apiPath: "url")
-        let session = RequestsBuilder().buildUrlSession()
-
-        MockedTasksExecutor.executefailed(session: session, request: request) { _, response in
-            XCTAssertNotNil(response)
-        }
-    }
-
-    @available(iOS 11.0, *)
-    func testPlaySuccess() throws {
-        var isSuccess = false
         let option = try Options(
-            mediaUrl: "https://vod.api.video/vod/vi21aJxFa0A1AFM6FPVmjnhA/hls/manifest.m3u8",
+            mediaUrl: "https://vod.api.video/vod/\(VideoIds.validVodVideoId)/hls/manifest.m3u8",
             metadata: [["string 1": "String 2"], ["string 3": "String 4"]],
             onSessionIdReceived: { _ in
             }
@@ -43,12 +16,67 @@ final class PlayerAnalyticsTests: XCTestCase {
         let api = PlayerAnalytics(options: option)
         api.play { result in
             switch result {
-            case .success:
-                isSuccess = true
-            case .failure:
-                isSuccess = false
+            case .success: expectationSuccess.fulfill()
+            default: break
             }
-            XCTAssertTrue(isSuccess)
         }
+        waitForExpectations(timeout: 10)
+    }
+
+    func testPlayLiveSuccess() throws {
+        let expectationSuccess = expectation(description: "Expect a success POST of play event")
+
+        let option = try Options(
+            mediaUrl: "https://live.api.video/\(VideoIds.validLiveStreamId).m3u8",
+            metadata: [["string 1": "String 2"], ["string 3": "String 4"]],
+            onSessionIdReceived: { _ in
+            }
+        )
+        let api = PlayerAnalytics(options: option)
+        api.play { result in
+            switch result {
+            case .success: expectationSuccess.fulfill()
+            default: break
+            }
+        }
+        waitForExpectations(timeout: 10)
+    }
+
+    func testPauseVodSuccess() throws {
+        let expectationSuccess = expectation(description: "Expect a success POST of pause event")
+
+        let option = try Options(
+            mediaUrl: "https://vod.api.video/vod/\(VideoIds.validVodVideoId)/hls/manifest.m3u8",
+            metadata: [["string 1": "String 2"], ["string 3": "String 4"]],
+            onSessionIdReceived: { _ in
+            }
+        )
+        let api = PlayerAnalytics(options: option)
+        api.pause { result in
+            switch result {
+            case .success: expectationSuccess.fulfill()
+            default: break
+            }
+        }
+        waitForExpectations(timeout: 10)
+    }
+
+    func testPauseLiveSuccess() throws {
+        let expectationSuccess = expectation(description: "Expect a success POST of pause event")
+
+        let option = try Options(
+            mediaUrl: "https://live.api.video/\(VideoIds.validLiveStreamId).m3u8",
+            metadata: [["string 1": "String 2"], ["string 3": "String 4"]],
+            onSessionIdReceived: { _ in
+            }
+        )
+        let api = PlayerAnalytics(options: option)
+        api.pause { result in
+            switch result {
+            case .success: expectationSuccess.fulfill()
+            default: break
+            }
+        }
+        waitForExpectations(timeout: 10)
     }
 }
