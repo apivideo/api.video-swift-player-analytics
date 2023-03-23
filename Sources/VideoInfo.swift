605045d95e0ab7cd352a9e4a7e938fa60e149da2
Romain Petit <contact.romain.petit@gmail.com>
2023-03-23 14:49:58 +0100

    feat() generate pingUrl on VideoInfo init

--- Sources/VideoInfo.swift
+++ Sources/VideoInfo.swift
@@ -1,7 +1,12 @@
 import Foundation
 
 public struct VideoInfo {
-    public let pingUrl: String
     public let videoId: String
     public let videoType: VideoType
+    public let pingUrl: String
+    public init(videoId: String, videoType: VideoType) {
+        self.videoId = videoId
+        self.videoType = videoType
+        self.pingUrl = "https://collector.api.video/\(videoType.rawValue)"
+    }
 }
